1e1884bfc862d6c06346c88b37f289315b6199da
rhkrqkq <rhkrqkq1025@gmail.com>
2025-05-12 18:06:45 +0900

    feat: Security 관련 파일

---
+++ src/main/java/com/sum_news_BE/security/CustomUserDetails.java
@@ -0,0 +1,56 @@
+package com.sum_news_BE.security;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.sum_news_BE.domain.User;
+
+import lombok.Getter;
+
+@Getter
+public class CustomUserDetails implements UserDetails {
+
+	private final User user;
+
+	public CustomUserDetails(User user) {
+		this.user = user;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return Collections.singleton(() -> "ROLE_USER");
+	}
+
+	@Override
+	public String getPassword() {
+		return user.getPassword();
+	}
+
+	@Override
+	public String getUsername() {  //userid 반환 (로그인에 사용할 유일한 값)
+		return user.getUserid();
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+}
