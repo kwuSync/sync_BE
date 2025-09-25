c7bd17a3661046a29b67e3b16a4f6cb2d7351c4b
rhkrqkq <rhkrqkq1025@gmail.com>
2025-09-25 11:54:19 +0900

    IP수정 및 배포 시 ResourceNotFound처리

--- src/main/java/com/sum_news_BE/config/CorsConfig.java
+++ src/main/java/com/sum_news_BE/config/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		CorsConfiguration config = new CorsConfiguration();
 
-		config.setAllowedOrigins(Arrays.asList("http://3.37.198.176:8080"));
+		config.setAllowedOrigins(Arrays.asList("http://52.79.237.72:8080", "http://localhost:3000", "http://localhost:8080"));
 
 		config.setAllowCredentials(true);
 		config.addAllowedHeader("*");
