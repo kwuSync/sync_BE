94cd1ff108ad55aa0e73c4333c33ac907b279b6e
rhkrqkq <rhkrqkq1025@gmail.com>
2025-10-28 02:56:27 +0900

    fix: CORS 사전 요청

--- src/main/java/com/sync_BE/config/SecurityConfig.java
+++ src/main/java/com/sync_BE/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.sync_BE.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -41,8 +42,9 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http
-			.addFilter(corsConfig.corsFilter())
+			.addFilterBefore(corsConfig.corsFilter(), org.springframework.security.web.context.SecurityContextHolderFilter.class)
 			.authorizeHttpRequests(authorize -> authorize
+				.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
 				.requestMatchers(url).permitAll()
 				.anyRequest().authenticated())
 			.csrf(csrf -> csrf.disable())
