b24cdeb3eeeb80128f12e304e7a58904037ebdf5
rhkrqkq <rhkrqkq1025@gmail.com>
2025-10-29 17:21:38 +0900

    refactor: DTO명 중복으로 인한 수정

--- src/main/java/com/sync_BE/web/dto/commentDTO/CommentRequestDTO.java
+++ src/main/java/com/sync_BE/web/dto/commentDTO/CommentRequestDTO.java
@@ -9,7 +9,7 @@ public class CommentRequestDTO {
     
     @Getter
     @Setter
-    public static class CreateDTO {
+    public static class CommentCreateDTO {
         @NotBlank(message = "댓글 내용은 필수 입력값입니다.")
         @Size(min = 1, max = 1000, message = "댓글은 1~1000자까지 입력 가능합니다.")
         private String commentText;
@@ -17,7 +17,7 @@ public class CommentRequestDTO {
     
     @Getter
     @Setter
-    public static class UpdateDTO {
+    public static class CommentUpdateDTO {
         @NotBlank(message = "댓글 내용은 필수 입력값입니다.")
         @Size(min = 1, max = 1000, message = "댓글은 1~1000자까지 입력 가능합니다.")
         private String commentText;
