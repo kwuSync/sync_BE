38ba94b2ca892bb90d77423c5ca526e6dc200587
rhkrqkq <rhkrqkq1025@gmail.com>
2025-10-31 03:06:59 +0900

    refactor: TTSService 누락 코드 추가

--- src/main/java/com/sync_BE/service/TTSService/TTSService.java
+++ src/main/java/com/sync_BE/service/TTSService/TTSService.java
@@ -1,6 +1,7 @@
 package com.sync_BE.service.TTSService;
 
 import java.io.IOException;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
@@ -13,6 +14,7 @@ import com.google.cloud.texttospeech.v1.TextToSpeechClient;
 import com.google.cloud.texttospeech.v1.VoiceSelectionParams;
 import com.google.protobuf.ByteString;
 import com.sync_BE.service.NewsService.NewsService;
+import com.sync_BE.web.dto.newsDTO.NewsResponseDTO;
 
 @Service
 public class TTSService {
@@ -24,12 +26,28 @@ public class TTSService {
 	}
 
 	public byte[] synthesizeMainSummary() throws IOException {
-		String summaryText = newsService.getNewsSummary();
+		NewsResponseDTO.NewsListDTO mainNewsList = newsService.getMain();
+
+		if (mainNewsList == null || mainNewsList.getNewsList() == null || mainNewsList.getNewsList().isEmpty()) {
+			throw new IOException("메인 요약 뉴스를 찾을 수 없습니다.");
+		}
+		NewsResponseDTO.NewsArticleDTO firstArticle = mainNewsList.getNewsList().get(0);
+
+		String summaryText = firstArticle.getSummaryText();
 		return synthesize(summaryText);
 	}
 
-	public byte[] synthesizeNewsSummary(String articleId) throws IOException {
-		Optional<>
+	public byte[] synthesizeNewsSummary(String clusterId) throws IOException {
+		NewsResponseDTO.NewsClusterDTO newsCluster = newsService.getNewsSummaryByClusterId(clusterId);
+		if (newsCluster == null || newsCluster.getSummary() == null || newsCluster.getSummary().getArticle() == null) { //
+			throw new IOException("요약된 뉴스를 찾을 수 없습니다.");
+		}
+		String summaryText = newsCluster.getSummary().getArticle();
+
+		if (summaryText.isEmpty()) {
+			throw new IOException("요약 텍스트가 비어있습니다.");
+		}
+		return synthesize(summaryText);
 	}
 
 	private byte[] synthesize(String text) throws IOException {
